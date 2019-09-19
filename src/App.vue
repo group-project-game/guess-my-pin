9d43c6cbcb684435a575e7709d22172a2b51c47b
fransnesa <Fransnesah@gmail.com>
2019-09-19 19:50:51 +0700

    tambah firebaseconfig

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view class="mt-5"></router-view>
   </div>
 </template>
 
